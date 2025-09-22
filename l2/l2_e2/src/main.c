6f2e66c20149e8a8bcc09980cad26180248a2f72
Cherie Yu <cherie.yyuu@gmail.com>
2025-09-22 20:09:14 +0200

    lab2 ex2

--- l2/l2_e2/src/main.c
+++ l2/l2_e2/src/main.c
@@ -21,41 +21,52 @@ static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 /* STEP 4 - Define the callback function */
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+  gpio_pin_toggle_dt(&led);
+}
 
 /* STEP 5 - Define a variable of type static struct gpio_callback */
+static struct gpio_callback button_cb_data;
 
 int main(void)
 {
-	int ret;
+  int ret;
 
-	if (!device_is_ready(led.port)) {
-		return -1;
-	}
+  if (!device_is_ready(led.port))
+  {
+    return -1;
+  }
 
-	if (!device_is_ready(button.port)) {
-		return -1;
-	}
+  if (!device_is_ready(button.port))
+  {
+    return -1;
+  }
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return -1;
-	}
+  ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (ret < 0)
+  {
+    return -1;
+  }
 
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret < 0) {
-		return -1;
-	}
-	/* STEP 3 - Configure the interrupt on the button's pin */
+  ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+  if (ret < 0)
+  {
+    return -1;
+  }
+  /* STEP 3 - Configure the interrupt on the button's pin */
+  ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
 
-	/* STEP 6 - Initialize the static struct gpio_callback variable   */
+  /* STEP 6 - Initialize the static struct gpio_callback variable   */
+  gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
 
-	/* STEP 7 - Add the callback function by calling gpio_add_callback()   */
+  /* STEP 7 - Add the callback function by calling gpio_add_callback()   */
+  gpio_add_callback(button.port, &button_cb_data);
 
-	while (1) {
-		/* STEP 8 - Remove the polling code */
-		bool val = gpio_pin_get_dt(&button);
-		gpio_pin_set_dt(&led, val);
+  while (1)
+  {
+    /* STEP 8 - Remove the polling code */
 
-		k_msleep(SLEEP_TIME_MS);
-	}
+    k_msleep(SLEEP_TIME_MS);
+  }
 }
